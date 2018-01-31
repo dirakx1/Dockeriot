80038ad579842532a0230ef3fc50e3931edda281
dirakx1 <35827025+dirakx1@users.noreply.github.com>
2018-01-30 20:19:51 -0500

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,1 +1,13 @@
+# Docker installation
 
+$ curl -s https://packagecloud.io/install/repositories/Hypriot/Schatzkiste/script.deb.sh | sudo bash
+$ sudo apt-get install docker-hypriot=1.10.3-1
+$ sudo sh -c 'usermod -aG docker $SUDO_USER'
+$ sudo systemctl enable docker.service
+$ systemctl unmask docker.service
+$ systemctl unmask docker.socket
+$ systemctl start docker.service
+# Verification
+$ sudo docker run hello-world   
+$ sudo docker info 
+$ docker rmi hello-world
